df133eb3eff91ae17432cd57a456dd618f123a96
Old_Shiba_Inu <draiver78@mail.ru>
2019-06-28 20:34:58 +0300

    Add files via upload

--- js/ajax.js
+++ js/ajax.js
@@ -19,7 +19,7 @@ function sendAjaxForm(result_form, ajax_form, url) {
         	$('#result_form').html('<a color="red">'+'Сообщение отправлено!'+'</a>'+'<img src="https://cdn.pixabay.com/photo/2013/07/13/10/08/green-156618_1280.png" width="25" height"25" alt="">');
     	},
     	error: function(response) { 
-            $('#result_form').html('<a color="red">'+'Ошибка. Данные не отправлены '+'</a>' + '<img src="http://auto-science.ru/wp-content/uploads/2016/06/g11.png" width="25" height"25" alt="">'+'<p color="red">GitHub-Pages не поддерживает php. Отправить можно тут <a href="https://vk.com/greedos">Зеркало</a> </p>');
+            $('#result_form').html('<a color="red">'+'Ошибка. Данные не отправлены '+'</a>' + '<img src="http://auto-science.ru/wp-content/uploads/2016/06/g11.png" width="25" height"25" alt="">'+'<p color="red">GitHub-Pages не поддерживает php. Отправить можно тут <a href="https://yaricklaba.000webhostapp.com/">Зеркало</a> </p>');
             
     	}
  	});
